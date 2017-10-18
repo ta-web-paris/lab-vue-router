71d985b323c34fb19e04fb8f4e124f933e3fa0bc
Nicolas <nicolas.barthez@gmail.com>
2017-10-18 15:31:52 +0200

    almost done

--- api.js
+++ api.js
@@ -1,11 +1,35 @@
 const service = axios.create({
-  baseURL: "http://localhost:3000/"
+  baseURL: "http://localhost:3000/movies"
 });
 
 const api = {
-  getAll: () => null, // Return instead a promise containing all the movies,
-  getOne: id => null, // Return instead a promise containing one movie,
-  addOne: info => null, // Make the request to add amovie and return a promise containing the added movie,
-  editOne: (id, info) => null, // Make the request to edit a movie and return a promise containing the edited movie,
-  deleteOne: id => null // Make the request to delete a movie
+  // Return instead a promise containing all the movies,
+  getAll: () => {
+    return service.get().then(response => {
+      return response.data;
+    });
+  },
+  // Return instead a promise containing one movie,
+  getOne: id => {
+    return service.get(id).then(resp => {
+      return resp.data;
+    });
+  },
+  // Make the request to add amovie and return a promise containing the added movie,
+  addOne: info => {
+    return service.post(info).then(resp => {
+      return resp.data;
+    });
+  },
+  editOne: (id, info) => {
+    return service.put(id, info).then(resp => {
+      return resp.data;
+    });
+  },
+  // Make the request to edit a movie and return a promise containing the edited movie,
+  deleteOne: id => {
+    return service.delete(id).then(resp => {
+      return resp;
+    });
+  } // Make the request to delete a movie
 };
---
+++ index.js
@@ -0,0 +1,90 @@
+const Home = {
+  data() {
+    return {
+      movies: []
+    };
+  },
+  template: `
+  <div>
+    <h1 class="title">Movies</h1>
+    <router-link :to="'/add'">
+      Add a new movie to the collection
+    </router-link>
+    <ul>
+      <li v-for="movie in movies">
+        <router-link :to="'/' + movie.id">
+            <movie-card :movie="movie" ></movie-card>
+        </router-link>
+      </li>
+    </ul>
+  </div>
+  `,
+
+  created() {
+    api.getAll().then(movies => {
+      this.movies = movies;
+    });
+  }
+};
+
+const MovieDetail = {
+  data() {
+    return {
+      movie: null
+    };
+  },
+  template: `
+  <div>
+    <div v-if="movie" >
+    <h1 class="title">{{movie.title}}</h1>
+    <img :src="movie.poster">
+    <p> <strong>Director: </strong>{{movie.director}}</p>
+    <p> <strong>Year: </strong> {{movie.year}}</p>
+    <p> <strong>Actors: </strong></p>
+    <ul>
+      <li v-for="actor in movie.actors"> {{actor}}   </li>
+    </ul>
+    <p> <strong>Genres: </strong></p>
+    <ul>
+      <li v-for="genre in movie.genres"> {{genre}}   </li>
+    </ul>
+    <p> <strong>Synopsis: </strong> {{movie.synopsis}}</p>
+    </div>
+  </div>
+  `,
+
+  created() {
+    api.getOne(this.$route.params.id).then(movie => {
+      this.movie = movie;
+    });
+  }
+};
+
+const AddMovie = {
+  data() {
+    return {
+      movie: null
+    };
+  },
+  template: `
+  <div>
+    <movie-form @submit="addOne()"></movie-form>
+  </div>
+  `
+};
+
+const routes = [
+  { path: "/", component: Home },
+  { path: "/add", component: AddMovie },
+  { path: "/:id", component: MovieDetail }
+];
+
+const router = new VueRouter({
+  mode: "history",
+  routes
+});
+
+new Vue({
+  el: "#app",
+  router
+});
