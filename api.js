976c66afa320f9a5beb2e3b438f176b727f75696
Jean-Nicolas Piketty <jnpiketty@BDLCM.local>
2018-02-21 14:59:47 +0100

    PAR WD vue router JN & Henri

--- api.js
+++ api.js
@@ -1,11 +1,11 @@
 const service = axios.create({
-  baseURL: 'http://localhost:3000/movies',
+  baseURL: "http://localhost:3000/movies"
 });
 
 const api = {
-  getAll: () => null, // Return instead a promise containing all the movies,
-  getOne: id => null, // Return instead a promise containing one movie,
-  addOne: info => null, // Make the request to add amovie and return a promise containing the added movie,
-  editOne: (id, info) => null, // Make the request to edit a movie and return a promise containing the edited movie,
-  deleteOne: id => null, // Make the request to delete a movie
+  getAll: () => service.get("/").then(res => res.data), // Return instead a promise containing all the movies,
+  getOne: id => service.get(`/${id}`).then(res => res.data), // Return instead a promise containing one movie,
+  addOne: info => service.post("/", info).then(res => info), // Make the request to add amovie and return a promise containing the added movie,
+  editOne: (id, info) => service.put(`/${id}`, info).then(res => info), // Make the request to edit a movie and return a promise containing the edited movie,
+  deleteOne: id => service.delete(`/${id}`) //.then(res => res) // Make the request to delete a movie
 };
